c65131d6e78f1489f951f39400bc6c2bc94758eb
rin-yato <chearithorn@gmail.com>
2023-02-06 23:34:22 +0700

    refactor: changing logic and style

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,14 +10,14 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Professional" | "Casual" | "Funny";
+export type VibeType = "Brand" | "Slogan";
 
 interface DropDownProps {
   vibe: VibeType;
   setVibe: (vibe: VibeType) => void;
 }
 
-let vibes: VibeType[] = ["Professional", "Casual", "Funny"];
+let vibes: VibeType[] = ["Brand", "Slogan"];
 
 export default function DropDown({ vibe, setVibe }: DropDownProps) {
   return (
